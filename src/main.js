b5db3d96acf03554e760b3e1e8fddc1b88d125cc
Akkorpy <umarbekalmayev@gmail.com>
2026-08-27 22:12:33 +0500

    Js done

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
 document.querySelector("#app").innerHTML = '<p>loading...</p>'
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
-    document.querySelector("#app").innerHTML = `
+    let media;
+    if (data.media_type === "image") {
+        media = `<img style="width: 300px; height: 200px;" src="${data.url}"/>`;
+    } else if(data.media_type === data.url.includes("youtube")){
+        media = `<iframe width="560" height="315" src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+    }
+     else {
+        media = `<video src="${data.url}" controls></video>`;
+    }
+    document.querySelector("#app").innerHTML = document.querySelector("#app").innerHTML = `
     <h1>${data.title}</h1>
-    <img src="${data.url}" alt="${data.title}" />
+    ${media}
     <p>${data.explanation}</p>
-  `
-}) 
+`;
+})
+.catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
