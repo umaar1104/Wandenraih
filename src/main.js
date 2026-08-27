1d0404b9d8615113341302ece6be33282c1af126
Akkorpy <umarbekalmayev@gmail.com>
2026-08-27 21:50:53 +0500

    knowledge

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
 document.querySelector("#app").innerHTML = '<p>loading...</p>'
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json()).then(data => {
-    console.log(data);
+    document.querySelector("#app").innerHTML = `
+    <h1>${data.title}</h1>
+    <img src="${data.url}" alt="${data.title}" />
+    <p>${data.explanation}</p>
+  `
 }) 
