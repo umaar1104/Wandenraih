60ac72f1ce0d7bbb48b937db3e6284a2f4005edc
Akkorpy <umarbekalmayev@gmail.com>
2026-08-27 21:27:21 +0500

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
-const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 document.querySelector("#app").innerHTML = '<p>loading...</p>'
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
